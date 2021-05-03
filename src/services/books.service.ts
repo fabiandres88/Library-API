7e0d63729c9d97281de13de17613896b0de58034
Fabian Ramirez <ramirez1988fabian@gmail.com>
2021-05-03 11:37:57 -0500

    feat: add book service

--- src/services/books.service.ts
+++ src/services/books.service.ts
@@ -1,7 +1,33 @@
-import { Service } from "typedi";
+import { IBook } from "../interfaces/book.interface";
+import { BooksRepository } from "../repositories/books.repository";
 
-
-@Service()
 export class BooksService {
-    constructor() {}
+    private  booksRepository: BooksRepository
+    constructor() { 
+        this.booksRepository= new BooksRepository()
+    }
+    async getAllBooks(): Promise<IBook[]> {
+        const books = await this.booksRepository.findAllBooks();
+        return books;
+    }
+
+    async getBookById(bookId: IBook["_id"]): Promise<IBook | null> {
+        const book = await this.booksRepository.findBookById(bookId);
+        return book;
+    }
+
+    async createBook(book: IBook): Promise<IBook> {
+        const createResult = await this.booksRepository.createBook(book);
+        return createResult;
+    }
+
+    async updateBook(bookId: IBook["_id"], fieldsToUpdate: any): Promise<IBook | null> {
+        const updateResult = await this.booksRepository.updateBookById(bookId, fieldsToUpdate);
+        return updateResult;
+    }
+
+    async deleteBook(bookId: IBook["_id"]): Promise<IBook | null> {
+        const removeResult = await this.booksRepository.removeBook(bookId);
+        return removeResult;
+    }
 }
