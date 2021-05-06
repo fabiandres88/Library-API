604551c79167ea47f1c105271aae3ae6179cdb70
Fabian Ramirez <ramirez1988fabian@gmail.com>
2021-05-05 21:32:08 -0500

    feat: add user service

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,6 +1,28 @@
-import { Service } from "typedi";
+import { IUser } from "../interfaces/user.interface";
+import { UsersRepository } from "../repositories/users.repository";
 
-@Service()
 export class UsersService {
-    constructor() {}
+    private  usersRepository: UsersRepository
+    constructor() { 
+        this.usersRepository= new UsersRepository()
+    }
+    async getUserById(userId: IUser["_id"]): Promise<IUser | null> {
+        const user = await this.usersRepository.findUserById(userId);
+        return user;
+    }
+
+    async createUser(user: IUser): Promise<IUser> {
+        const createResult = await this.usersRepository.createUser(user);
+        return createResult;
+    }
+
+    async updateUser(userId: IUser["_id"], fieldsToUpdate: any): Promise<IUser | null> {
+        const updateResult = await this.usersRepository.updateUserById(userId, fieldsToUpdate);
+        return updateResult;
+    }
+
+    async deleteUser(userId: IUser["_id"]): Promise<IUser | null> {
+        const removeResult = await this.usersRepository.removeUser(userId);
+        return removeResult;
+    }
 }
